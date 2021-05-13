3d7cb3828e49c71529ce25d8506497670a18728f
Ben Dan <bendan7@gmail.com>
2021-05-13 18:30:20 +0300

    Add background

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,58 +1,75 @@
-import React from "react";
-import Signup from "../pages/Signup";
-import UserDashboard from "../pages/UserDashboard";
-import Login from "../pages/Login";
-import { AuthProvider } from "../contexts/AuthContext";
-import { MeetingProvider } from "../contexts/MeetingContext";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-import ForgotPassword from "../pages/ForgotPassword";
-import UpdateProfile from "../pages/UpdateProfile";
-import PrivateAdminRoute from "./PrivateAdminRoute";
-import AdminDashboard from "../pages/admin/AdminDashboard";
-import Meetings from "../pages/admin/Meetings";
+import React from 'react'
+import Signup from '../pages/Signup'
+import UserDashboard from '../pages/UserDashboard'
+import Login from '../pages/Login'
+import { AuthProvider } from '../contexts/AuthContext'
+import { MeetingProvider } from '../contexts/MeetingContext'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
+import ForgotPassword from '../pages/ForgotPassword'
+import UpdateProfile from '../pages/UpdateProfile'
+import PrivateAdminRoute from './PrivateAdminRoute'
+import AdminDashboard from '../pages/admin/AdminDashboard'
+import Meetings from '../pages/admin/Meetings'
 
 export default function App() {
-  const centerImg = {
-    width: "25vw",
-    maxWidth: "120px",
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
-    marginBottom: "2vh",
-  };
+    const centerImg = {
+        width: '25vw',
+        maxWidth: '120px',
+        display: 'block',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: '0.8vh',
+        marginTop: '2vh',
+    }
 
-  return (
-    <div
-      className="d-flex align-items-top justify-content-center w-100 p-3"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "800px" }}>
-        <Router>
-          <Link to="/">
-            <img
-              alt="logo"
-              style={centerImg}
-              src={process.env.PUBLIC_URL + "/logo.png"}
-            />
-          </Link>
-          <h3 className="text-center">מערכת ניהול כרטיסיות</h3>
-          <AuthProvider>
-            <MeetingProvider>
-              <Switch>
-                <Route path="/signup" component={Signup} />
-                <Route path="/login" component={Login} />
-                <Route path="/forgot-password" component={ForgotPassword} />
-                <Route path="/update-profile" component={UpdateProfile} />
+    return (
+        <div
+            className="d-flex align-items-top justify-content-center w-100 p-3"
+            style={{ minHeight: '100vh' }}
+        >
+            <div className="w-100" style={{ maxWidth: '800px' }}>
+                <Router>
+                    <Link to="/">
+                        <img
+                            alt="logo"
+                            style={centerImg}
+                            src={process.env.PUBLIC_URL + '/logo.png'}
+                        />
+                    </Link>
+                    <h3 className="text-center">מערכת ניהול כרטיסיות</h3>
+                    <AuthProvider>
+                        <MeetingProvider>
+                            <Switch>
+                                <Route path="/signup" component={Signup} />
+                                <Route path="/login" component={Login} />
+                                <Route
+                                    path="/forgot-password"
+                                    component={ForgotPassword}
+                                />
+                                <Route
+                                    path="/update-profile"
+                                    component={UpdateProfile}
+                                />
 
-                <PrivateRoute exact path="/" component={UserDashboard} />
-                <PrivateAdminRoute path="/admin" component={AdminDashboard} />
-                <PrivateAdminRoute path="/meetings" component={Meetings} />
-              </Switch>
-            </MeetingProvider>
-          </AuthProvider>
-        </Router>
-      </div>
-    </div>
-  );
+                                <PrivateRoute
+                                    exact
+                                    path="/"
+                                    component={UserDashboard}
+                                />
+                                <PrivateAdminRoute
+                                    path="/admin"
+                                    component={AdminDashboard}
+                                />
+                                <PrivateAdminRoute
+                                    path="/meetings"
+                                    component={Meetings}
+                                />
+                            </Switch>
+                        </MeetingProvider>
+                    </AuthProvider>
+                </Router>
+            </div>
+        </div>
+    )
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,78 +1,90 @@
-import React, { useRef, useState, useEffect } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import React, { useRef, useState, useEffect } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Login() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const { login, currentUser } = useAuth();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const history = useHistory();
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const { login, currentUser } = useAuth()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState()
+    const history = useHistory()
 
-  useEffect(() => {
-    if (currentUser) {
-      history.push("/");
+    useEffect(() => {
+        if (currentUser) {
+            history.push('/')
+        }
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        try {
+            setError('')
+            setLoading(true)
+            await login(emailRef.current.value, passwordRef.current.value)
+        } catch (e) {
+            setError(`${e?.message} `)
+        } finally {
+            setLoading(false)
+        }
     }
-  });
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+    return (
+        <>
+            <Card>
+                <Card.Body className="text-center">
+                    <h2>התחברות</h2>
+                    <Form onSubmit={handleSubmit} className="text-right">
+                        <Form.Group>
+                            <Form.Label>דואר אלקטרוני</Form.Label>
+                            <Form.Control
+                                id="email"
+                                type="email"
+                                ref={emailRef}
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>סיסמא</Form.Label>
+                            <Form.Control
+                                id="password"
+                                type="password"
+                                ref={passwordRef}
+                                required
+                            />
+                        </Form.Group>
+                        <Button
+                            disabled={loading}
+                            className="w-100 "
+                            type="submit"
+                        >
+                            התחברות
+                        </Button>
+                    </Form>
+                    <div className="w-100 text-center mt-3">
+                        <Link to="forgot-password">? שכחת סיסמה</Link>
+                    </div>
+                    <div className="w-100 text-center mt-2">
+                        אין לך חשבון? <Link to="/signup">צור חשבון חדש</Link>
+                    </div>
+                    <div className="m-4 p-2 border bg-warning">
+                        <h3>Demo Credentials</h3>
+                        <div>User: nofar@gmail.com Pass: 123456</div>
+                        <div>Admin: bendan7@gmail.com Pass: 123456</div>
+                    </div>
 
-    try {
-      setError("");
-      setLoading(true);
-      await login(emailRef.current.value, passwordRef.current.value);
-    } catch (e) {
-      setError(`${e?.message} `);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  return (
-    <>
-      <Card>
-        <Card.Body className="text-center">
-          <h2>התחברות</h2>
-          <Form onSubmit={handleSubmit} className="text-right">
-            <Form.Group>
-              <Form.Label>דואר אלקטרוני</Form.Label>
-              <Form.Control id="email" type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>סיסמא</Form.Label>
-              <Form.Control
-                id="password"
-                type="password"
-                ref={passwordRef}
-                required
-              />
-            </Form.Group>
-            <Button disabled={loading} className="w-100 " type="submit">
-              התחברות
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="forgot-password">? שכחת סיסמה</Link>
-          </div>
-          <div className="pt-5">
-            <h5>Demo</h5>
-            <p>User: nofar@gmail.com Pass: 123456</p>
-            <p>Admin: bendan7@gmail.com Pass: 123456</p>
-          </div>
-
-          {error && (
-            <Alert className=" mt-3 mb-0 text-center" variant="danger">
-              {error}
-            </Alert>
-          )}
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        אין לך חשבון? <Link to="/signup">צור חשבון חדש</Link>
-      </div>
-    </>
-  );
+                    {error && (
+                        <Alert
+                            className=" mt-3 mb-0 text-center"
+                            variant="danger"
+                        >
+                            {error}
+                        </Alert>
+                    )}
+                </Card.Body>
+            </Card>
+        </>
+    )
 }
