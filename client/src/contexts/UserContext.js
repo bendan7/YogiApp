3fc63be370eda2a01444c00643fe6243010bbfd5
Ben Dan <bendan7@gmail.com>
2021-05-16 11:06:50 +0300

    Add baseUrl Var to .env

--- client/src/contexts/UserContext.js
+++ client/src/contexts/UserContext.js
@@ -1,116 +1,120 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useAuth } from "./AuthContext";
-import { useMeetingsContext } from "./MeetingContext";
+import React, { useContext, useEffect, useState } from 'react'
+import { useAuth } from './AuthContext'
+import { useMeetingsContext } from './MeetingContext'
 
-const UserContext = React.createContext();
+const UserContext = React.createContext()
 
 export function useUserContext() {
-  return useContext(UserContext);
+    return useContext(UserContext)
 }
 
 export function UserProvider({ children }) {
-  const { currentUser, GetHttpReq } = useAuth();
+    const { currentUser, GetHttpReq } = useAuth()
 
-  //upcoming meetings list
-  const { meetings } = useMeetingsContext();
+    //upcoming meetings list
+    const { meetings } = useMeetingsContext()
 
-  // registered upcoming meetings ids list
-  const [registered, setRegistered] = useState([]);
+    // registered upcoming meetings ids list
+    const [registered, setRegistered] = useState([])
 
-  // user history is list of meeting and tickt purchases
-  // the sum of left entries is calc by the user history AllTickts-AllMeetings
-  const [userHistory, setUserHistory] = useState([]);
-  const [userEntries, setUserEntries] = useState(0);
+    // user history is list of meeting and tickt purchases
+    // the sum of left entries is calc by the user history AllTickts-AllMeetings
+    const [userHistory, setUserHistory] = useState([])
+    const [userEntries, setUserEntries] = useState(0)
 
-  function FindUserRegisteredMeetings(meetingList) {
-    const RegList = [];
+    function FindUserRegisteredMeetings(meetingList) {
+        const RegList = []
 
-    meetingList?.forEach((meeting) => {
-      meeting.participates?.forEach((par) => {
-        if (par.uid === currentUser.uid) {
-          RegList.push(meeting.id);
+        meetingList?.forEach((meeting) => {
+            meeting.participates?.forEach((par) => {
+                if (par.uid === currentUser.uid) {
+                    RegList.push(meeting.id)
+                }
+            })
+        })
+
+        return RegList
+    }
+
+    async function GetUserInfo() {
+        const req = await GetHttpReq()
+        req.method = 'GET'
+
+        fetch(`${process.env.REACT_APP_BASE_URL}/api/userhistory`, req)
+            .then((res) => {
+                res.json()
+                    .then((data) => {
+                        data.meetings.forEach((meeting) => {
+                            meeting.type = 'meeting'
+                            meeting.date = new Date(
+                                meeting.date._seconds * 1000
+                            )
+                        })
+
+                        let totalPurchasedEntries = 0
+                        data.tickts.forEach((tickt) => {
+                            tickt.type = 'tickt'
+                            totalPurchasedEntries += tickt.num_of_entries
+                            tickt.date = new Date(tickt.date?._seconds * 1000)
+                        })
+
+                        const validEntries =
+                            totalPurchasedEntries - data.meetings.length
+                        setUserEntries(validEntries)
+
+                        const history = data.meetings.concat(data.tickts)
+                        history.sort((a, b) => b.date - a.date)
+
+                        setUserHistory(history)
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            })
+            .catch((err) => {
+                console.log('faild to get user info')
+            })
+    }
+
+    async function RegisterMeeting(meeting) {
+        const httpReq = await GetHttpReq()
+        httpReq.method = 'PUT'
+        return fetch(
+            `${process.env.REACT_APP_BASE_URL}/api/meetings/register/${meeting.id}`,
+            httpReq
+        )
+    }
+
+    async function UnregisterFromMeeting(meeting) {
+        const httpReq = await GetHttpReq()
+        httpReq.method = 'PUT'
+        return fetch(
+            `${process.env.REACT_APP_BASE_URL}/api/meetings/deregister/${meeting.id}`,
+            httpReq
+        )
+    }
+
+    // On component mount
+    useEffect(() => {
+        if (currentUser) {
+            GetUserInfo()
         }
-      });
-    });
+    }, [])
 
-    return RegList;
-  }
+    // On meetings change
+    useEffect(() => {
+        if (meetings) {
+            setRegistered(FindUserRegisteredMeetings(meetings))
+        }
+    }, [meetings])
 
-  async function GetUserInfo() {
-    const req = await GetHttpReq();
-    req.method = "GET";
-
-    fetch(
-      "/api/userhistory",
-      req
-    )
-      .then((res) => {
-        res.json().then((data) => {
-          data.meetings.forEach((meeting) => {
-            meeting.type = "meeting";
-            meeting.date = new Date(meeting.date._seconds * 1000);
-          });
-
-          let totalPurchasedEntries = 0;
-          data.tickts.forEach((tickt) => {
-            tickt.type = "tickt";
-            totalPurchasedEntries += tickt.num_of_entries;
-            tickt.date = new Date(tickt.date?._seconds * 1000);
-          });
-
-          const validEntries = totalPurchasedEntries - data.meetings.length;
-          setUserEntries(validEntries);
-
-          const history = data.meetings.concat(data.tickts);
-          history.sort((a, b) => b.date - a.date);
-
-          setUserHistory(history);
-        });
-      })
-      .catch((err) => {
-        console.log("faild to get user info");
-      });
-  }
-
-  async function RegisterMeeting(meeting) {
-    const httpReq = await GetHttpReq();
-    httpReq.method = "PUT";
-    return fetch(
-      `/api/meetings/register/${meeting.id}`,
-      httpReq
-    );
-  }
-
-  async function UnregisterFromMeeting(meeting) {
-    const httpReq = await GetHttpReq();
-    httpReq.method = "PUT";
-    return fetch(
-      `/api/meetings/deregister/${meeting.id}`,
-      httpReq
-    );
-  }
-
-  // On component mount
-  useEffect(() => {
-    if (currentUser) {
-      GetUserInfo();
+    const value = {
+        registered,
+        userHistory,
+        userEntries,
+        RegisterToMeeting: RegisterMeeting,
+        UnregisterFromMeeting,
     }
-  }, []);
 
-  // On meetings change
-  useEffect(() => {
-    if (meetings) {
-      setRegistered(FindUserRegisteredMeetings(meetings));
-    }
-  }, [meetings]);
-
-  const value = {
-    registered,
-    userHistory,
-    userEntries,
-    RegisterToMeeting: RegisterMeeting,
-    UnregisterFromMeeting,
-  };
-
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
